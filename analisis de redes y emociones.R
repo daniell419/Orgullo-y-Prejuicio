8569f1eef0e242a0c9c27165b39afa2821005598
Daniel Lasso <111817055+daniell419@users.noreply.github.com>
2024-03-25 18:21:47 -0500

    asa

--- analisis de redes y emociones.R
+++ analisis de redes y emociones.R
@@ -17,14 +17,31 @@ setwd("C:/Users/danie/OneDrive/Documents/GitHub/Orgullo-y-Prejuicio")
 
 # Leer el archivo de texto de Orgullo y Prejuicio
 texto_OYP <- read.table("OYP.txt", fileEncoding = "UTF-8", sep = "\n", allowEscapes = T)
-# tokenizar las palabras
 
-texto_palabrasOYP <- get_tokens(texto_OYP)
+# Obtener lista de stopwords en español
+stopwords <- stopwords("spanish")
+
+# Convertir texto a corpus
+corpus_OYP <- Corpus(VectorSource(texto_OYP))
+
+# Eliminar stopwords del corpus
+corpus_OYP_sin_stopwords <- tm_map(corpus_OYP, removeWords, stopwords)
+
+# Convertir corpus procesado a texto
+texto_sin_stopwords <- sapply(corpus_OYP_sin_stopwords, as.character)
+
+# tokenizar las palabras sin stopwords
+texto_palabrasOYP <- get_tokens(texto_sin_stopwords)
+
 # extraer los sentimientos asociados a cada token
 sentimientosOYP_df <- get_nrc_sentiment(texto_palabrasOYP, lang="spanish")
 
-colnames(sentimientosOYP_df) <- c('Rabia', 'Anticipaci?n', 'disgusto', 'miedo', "Alegr?a","Tristeza","sorpresa", "confianza", "negative", "positive")
 
+# vector de los nombres de las columnas
+colnames(sentimientosOYP_df) <- c('Rabia', 'Anticipacion', 'disgusto', 'miedo', "Alegria","Tristeza","sorpresa", 
+                                  "confianza", "negativo", "positivo")
+
+# barplot de los sentimientos
 barplot(
   colSums(prop.table(sentimientosOYP_df[, 1:8])),
   space = 0.2,
@@ -32,53 +49,51 @@ barplot(
   las = 1,
   cex.names = 0.7,
   col = brewer.pal(n = 8, name = "Set3"),
-  main = "An?lisis de emociones en el libro: 'ORGULLO y PREJUICIO' de Jane Austen",
-  xlab="Emociones", ylab = "Frecuencia relativa de aparici?n")
+  main = "Analisis de emociones en el libro: 'ORGULLO y PREJUICIO' de Jane Austen",
+  xlab="Emociones", ylab = "Frecuencia relativa de la emoción")
 
-sentimientos_valencia <- (sentimientosOYP_df$negative *-1) + sentimientosOYP_df$positive
+
+## plot sobre el desarrollo de los sentimientos a traves del tiempo de la novela
+sentimientos_valencia <- (sentimientosOYP_df$negativo *-1) + sentimientosOYP_df$positivo
 simple_plot(sentimientos_valencia)
 
-sentimientos_OvsP <- (sentimientosOYP_df$disgusto*-1) + sentimientosOYP_df$confianza
-simple_plot(sentimientos_OvsP)
+# Calcular puntuaciones de sentimientos
+sentimientosOYP_df <- get_nrc_sentiment(texto_palabrasOYP, lang="spanish")
 
-palabras_tristeza <- texto_OYP[sentimientosOYP_df$Rabia > 0, 1]
 
-palabras_tristeza_orden <- sort(table(unlist(palabras_tristeza)), decreasing = TRUE)
-head(palabras_tristeza_orden, n = 12)
-## extraer las palabras que generan cada una de las emociones, hay que a?adir al codigo el n?mero de la columna. 
+# vector de los nombres de las columnas
+colnames(sentimientosOYP_df) <- c('Rabia', 'Anticipacion', 'disgusto', 'miedo', "Alegria","Tristeza","sorpresa", 
+                                  "confianza", "negativo", "positivo")
+
+head(sentimientosOYP_df)
+
+palabras_tristes <- texto_palabrasOYP[sentimientosOYP_df$Tristeza> 0]
+
+
+# Extraer palabras por emoción
 nube_emociones_vector <- c(
-  paste(texto_OYP[sentimientosOYP_df$confianza > 0, 8], collapse = " "),
-  paste(texto_OYP[sentimientosOYP_df$Rabia > 0, 1], collapse = " "),
-  paste(texto_OYP[sentimientosOYP_df$Tristeza > 0, 6], collapse = " "),
-  paste(texto_OYP[sentimientosOYP_df$miedo > 0, 4], collapse = " "),
-  paste(texto_OYP[sentimientosOYP_df$"Alegr?a" > 0, 5], collapse = " "))
+  paste(texto_palabrasOYP[sentimientosOYP_df$Rabia > 0], collapse = " "),
+  paste(texto_palabrasOYP[sentimientosOYP_df$Tristeza > 0], collapse = " "),
+  paste(texto_palabrasOYP[sentimientosOYP_df$miedo > 0], collapse = " "),
+  paste(texto_palabrasOYP[sentimientosOYP_df$Alegria > 0], collapse = " "))
 
 
-nube_emociones_vector <- iconv(nube_emociones_vector, "latin1", "UTF-8")
-nube_tdm <- gsub(words, pattern = '[[:punct:][:digit:]]', replacement = '') 
-nube_corpus <- Corpus(VectorSource(nube_emociones_vector))
+# Convertir el vector de palabras a codificación UTF-8
+nube_emociones_vector_2 <- iconv(nube_emociones_vector, "latin1", "UTF-8")
+
+# Crear corpus y matriz de términos-documento
+nube_corpus <- Corpus(VectorSource(nube_emociones_vector_2))
 nube_tdm <- TermDocumentMatrix(nube_corpus)
 nube_tdm <- as.matrix(nube_tdm)
-colnames(nube_tdm) <- c('Confianza', 'Rab?a', 'Tristeza', 'Miedo', "Alegr?a")
+
+# Asignar nombres de columnas
+colnames(nube_tdm) <- c('Rabia', 'Tristeza', 'Miedo', "Alegría")
+
+# Generar nube de palabras
 comparison.cloud(nube_tdm, random.order = FALSE,
                  colors = c("green", "red", "orange", "blue"),
-                 title.size = 1, max.words = 50, scale = c(2.5, 1), rot.per = 0.4)
+                 title.size = 1, max.words = 200, scale = c(2.5, 1), rot.per = 0.4)
 
-## Cargar la base en inglés:
-texto_OYPingl <- read.table("D:/OYP/Orgullo-y-Prejuicio/OYPingles.txt", fileEncoding = "UTF-8", sep = "\n", allowEscapes = T)
-texto_palabrasOYPingl <- get_tokens(texto_OYP)
-sentimientosOYPingl_df <- get_nrc_sentiment(texto_palabrasOYP, lang="english")
-
-colnames(sentimientosOYPingl_df) <- c('Rabia', 'Anticipaci?n', 'disgusto', 'miedo', "Alegr?a","Tristeza","sorpresa", "confianza", "negative", "positive")
-
-barplot(
-  colSums(prop.table(sentimientosOYPingl_df[, 1:8])),
-  space = 0.2,
-  horiz = FALSE,
-  las = 1,
-  cex.names = 0.7,
-  col = brewer.pal(n = 8, name = "Set3"),
-  main = "An?lisis de emociones en el libro: 'ORGULLO y PREJUICIO' de Jane Austen",
-  xlab="Emociones", ylab = "Frecuencia relativa de aparici?n")
-
-## Gracias!
+###############
+## Gracias! ##
+##############
